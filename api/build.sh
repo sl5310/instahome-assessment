132f3fd05657103c642f61b78c331f8a38cbd131
sltee <sl5310520@hotmail.com>
2021-02-10 22:36:49 +0800

    build

--- api/build.sh
+++ api/build.sh
@@ -1,4 +1,5 @@
+yarn build
 aws ecr get-login-password --region ap-southeast-1 --profile redbeat-cli | docker login --username AWS --password-stdin 411505671193.dkr.ecr.ap-southeast-1.amazonaws.com
-docker build -t redbeat:1.0 .
-docker tag redbeat:1.0 411505671193.dkr.ecr.ap-southeast-1.amazonaws.com/redbeat:1.0
-docker push 411505671193.dkr.ecr.ap-southeast-1.amazonaws.com/redbeat:1.0
+docker build -t instahome:1.1 .
+docker tag instahome:1.1 411505671193.dkr.ecr.ap-southeast-1.amazonaws.com/instahome:1.1
+docker push 411505671193.dkr.ecr.ap-southeast-1.amazonaws.com/instahome:1.1
